934784433d3d24ed8c8c447406add05e6b075aee
Andrey Kaizer <andre.kaizer@yandex.ru>
2022-11-08 00:42:45 +0300

    Создание файла ignore, классов Main, Basket, метод добавления - addToCart(int productNum, int amount),  метод сохранения - saveTxt(File textFile), статический метод - static Basket loadFromTxtFile(File textFile)

---
+++ src/Basket.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Basket {
+    protected String[] productNames;
+    protected int[] prices;
+    protected int[] counts;
+
+    public Basket(String[] productNames, int[] prices) {
+        this.productNames = productNames;
+        this.prices = prices;
+        this.counts = new int[prices.length];
+    }
+
+    private Basket(String[] productNames, int[] prices, int[] counts) {
+        this.productNames = productNames;
+        this.prices = prices;
+        this.counts = counts;
+    }
+
+    public void addToCart(int productNum, int amount) {
+        counts[productNum] = +amount;
+    }
+
+    public void printCart() {
+
+    }
+
+    public void saveTxt(File textFile) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String productName : productNames) {
+                out.print(productName + " ");
+            }
+            out.println();
+            for (int price : prices) {
+                out.print(price + " ");
+            }
+            out.println();
+            for (int count : counts) {
+                out.print(count + " ");
+            }
+            out.println();
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream(textFile))) {
+            String[] productNames = scanner.nextLine().trim().split(" ");
+            int[] prices = Arrays.stream(scanner.nextLine().trim().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int[] counts = Arrays.stream(scanner.nextLine().trim().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            return new Basket(productNames, prices, counts);
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String[] productNames = {"Хлеб", "Молоко", "Масло"};
+        int[] prices = {25, 100, 115};
+
+        Basket basket = new Basket(productNames, prices);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите товар и количество: ");
+            String line = scanner.nextLine();
+            if (line.equals("end")) {
+                break;
+            }
+            String[] parts = line.split(" ");
+            int productNum = Integer.parseInt(parts[0]) - 1;
+            int count = Integer.parseInt(parts[1]);
+            basket.addToCart(productNum, count);
+        }
+        basket.saveTxt(new File("basket.txt"));
+
+        basket.printCart();
+    }
+}
+
